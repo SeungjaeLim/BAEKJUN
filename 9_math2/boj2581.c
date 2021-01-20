f192611d3f97b477c1d543cec748d67736ec7a36
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-21 03:22:24 +0900

    Solve Boj11653

--- 9_math2/boj2581.c
+++ 9_math2/boj2581.c
@@ -1,26 +1,14 @@
 #include<stdio.h>
+#include<math.h>
+
+void eratos(int* is_prime, int n);
+
 int main()
 {
     int m,n, is_prime[10001] = {0, };
     int sum =  0, min_prime = -1;
     scanf("%d %d",&m,&n);
-    for(int i = 2; i < 10001 ; i++)
-    {
-        is_prime[i] = 1;
-    }
-    for(int i = 2 ; i < 101 ; i++)
-    {
-        if(is_prime[i] == 1)
-        {
-            for(int j = i*i ; j < 10001 ; j++ )
-            {
-                if(j%i == 0)
-                {
-                    is_prime[j] = 0;
-                }
-            }
-        }
-    }
+    eratos(is_prime, 10001);
     for(int i = m ; i <= n ; i++)
     {
         if(is_prime[i])
@@ -40,4 +28,26 @@ int main()
     {
         printf("%d\n%d",sum, min_prime);
     }
+}
+
+void eratos(int* is_prime, int n)
+{
+    for(int i = 2; i < n ; i++)
+    {
+        is_prime[i] = 1;
+    }
+    for(int i = 2 ; i < sqrt(n) ; i++)
+    {
+        if(is_prime[i] == 1)
+        {
+            for(int j = i*i ; j < 10001 ; j++ )
+            {
+                if(j%i == 0)
+                {
+                    is_prime[j] = 0;
+                }
+            }
+        }
+    }
+    return;
 }
