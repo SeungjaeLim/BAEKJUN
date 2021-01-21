8ac7807844a139cb064c277fefa1609fc4cf069a
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-22 01:24:39 +0900

    Solve boj3053

---
+++ 9_math2/boj3053.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#define _USE_MATH_DEFINES
+#include<math.h>
+
+
+int main()
+{
+    double r;
+    scanf("%lf",&r);
+    printf("%lf\n", r*r*M_PI);
+    printf("%lf", r * r * 2);
+}
