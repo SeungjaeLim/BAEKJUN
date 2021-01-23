944ac2bca5a72617c4dd1da15bf5268c31099521
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-24 01:32:50 +0900

    Solve boj2231

---
+++ 11_brute_force/boj2231.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int constructor(n);
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",constructor(n));
+    return 0;
+}
+
+int constructor(int n)
+{
+    int i = 0, tmp;
+    int construct = 0;
+    while(i<1000000)
+    {
+        tmp = i;
+        construct = tmp;
+        while(tmp != 0)
+        {
+            construct = construct + tmp%10;
+            tmp = tmp / 10;
+        }
+        if(construct == n)
+        {
+            return i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return 0;
+}
