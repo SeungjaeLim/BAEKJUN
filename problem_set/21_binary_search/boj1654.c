31b3ddaa2169a1fba989c7722766ca2785ffb5b5
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-07-18 03:38:07 +0900

    Solve binary search

---
+++ problem_set/21_binary_search/boj1654.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#define INT_MAX 2147483647
+
+long long int line[10000];
+
+long long int cut_bsearch(long long int k, long long int n, long long int left, long long int right);
+
+int main()
+{
+    long long int k, n;
+    scanf("%lld %lld", &k, &n);
+    for(int i = 0 ; i < k ; i++)
+    {
+        scanf("%lld", &line[i]);
+    }
+    printf("%lld", cut_bsearch(k, n, 0, INT_MAX));
+    return 0;
+}
+
+long long int cut_bsearch(long long int k, long long int n, long long int left, long long int right)
+{
+    long long int sum = 0;
+    long long int mid = (left + right) / 2;
+    //printf("%d %d %d\n", mid, left, right);
+    if(left > right)
+    {
+        return mid;
+    }
+    if(mid == 0)
+    {
+        return right;
+    }
+    for(int i = 0 ; i < k; i++)
+    {
+        sum += line[i] / mid;
+    }
+    //printf("%d %d\n",sum, n);
+    if(sum >= n)
+    {
+        return cut_bsearch(k, n , mid+1, right);
+    }
+    else
+    {
+        return cut_bsearch(k, n, left, mid-1);
+    }
+}
