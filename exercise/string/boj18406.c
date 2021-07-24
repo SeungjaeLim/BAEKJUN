81d7ca2787ff4ddab4b0e1f3cb343f843b8fddfa
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-07-24 19:07:28 +0900

    Solve boj18406

---
+++ exercise/string/boj18406.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int Lscore = 0, Rscore = 0;
+    char* score = (char*)calloc(sizeof(char), 9);
+    scanf("%s", score);
+    int len = strlen(score);
+    for(int i = 0; i < len/2; i++)
+    {
+        Lscore += *(score + i) + '0';
+        Rscore += *(score + len - 1 - i) + '0';
+    }
+    if(Lscore == Rscore)
+    {
+        printf("LUCKY");
+    }
+    else
+    {
+        printf("READY");
+    }
+    return 0;
+}
